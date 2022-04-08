ac0d9b5d9a68d5f90ede83b34a9a55237ee249ca
Enrico D'Angelo <edangelo@ptc.com>
2022-04-08 17:54:00 +0100

    todolist + errors

--- src/domain/entity/TodoList.ts
+++ src/domain/entity/TodoList.ts
@@ -1,42 +1,77 @@
-import { isStoredTask, StoredTask, Task } from './Task';
+import { CleanPocError, CLEANPOC_ERROR } from '../../utils/error/CleanPocErrors';
+import { isStoredTask, Task } from './Task';
 import { TaskId } from './TaskId';
 import { TodoListId } from './TodoListId';
 
 export class TodoList {
-  private _name: string;
-  private _tasks: Task[];
+    private _name: string;
+    private _tasks: Task[];
 
-  protected constructor(name: string, tasks: Task[]) {
-    this._name = name;
-    this._tasks = tasks;
-  }
-
-  static createNewTodoList(name: string): TodoList {
-    return new TodoList(name, []);
-  }
-  moveTaskToPos(taskId: TaskId, newPos: number): void {
-    const task: Task = this._tasks.filter((curTask: Task) => {
-      return isStoredTask(task) && task.id.equals(taskId);
-    });
-    // TODO check that I found 1 task
-    if (task.pos === newPos) {
-      return;
+    protected constructor(name: string, tasks: Task[]) {
+        this._name = name;
+        this._tasks = tasks;
     }
-    const oldPos = task.pos;
-    task.pos = newPos;
-    // TODO move all task with pos <= newpos to newPos-1
-  }
+
+    static createNewTodoList(name: string): TodoList {
+        return new TodoList(name, []);
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get tasks(): Task[] {
+        return this._tasks;
+    }
+
+    moveTaskToPos(taskId: TaskId, newPos: number): void {
+        if (this._tasks.length === 0) {
+            throw new CleanPocError(CLEANPOC_ERROR.ENTITY_STATUS_ERROR, `No tasks, todoListId: ${isStoredTodoList(this) ? this.id : 'NOT_PERSISTED'}`);
+        }
+        if (newPos > this._tasks.length) {
+            newPos = this._tasks.length;
+        } else if (newPos < 0) {
+            newPos = 0;
+        }
+        const tasks: Task[] = this._tasks.filter((curTask: Task) => {
+            return isStoredTask(curTask) && curTask.id.equals(taskId);
+        });
+        if (tasks.length > 1) {
+            throw new CleanPocError(
+                CLEANPOC_ERROR.ENTITY_STATUS_ERROR,
+                `More than 1 task with same id, todoListId: ${isStoredTodoList(this) ? this.id : 'NOT_PERSISTED'}, taskId: ${taskId}`,
+            );
+        } else if (tasks.length === 0) {
+            throw new CleanPocError(
+                CLEANPOC_ERROR.ENTITY_STATUS_ERROR,
+                `No task with this id in this list, todoListId: ${isStoredTodoList(this) ? this.id : 'NOT_PERSISTED'}, taskId: ${taskId}`,
+            );
+        }
+        const task = tasks[0];
+        if (task.pos === newPos) {
+            return;
+        }
+        const oldPos = task.pos;
+        task.pos = newPos;
+        this._tasks = this._tasks.map((curTask: Task) => {
+            if (curTask.pos > oldPos && curTask.pos <= newPos) {
+                curTask.pos--;
+            }
+            return curTask;
+        });
+        task.pos = newPos;
+    }
 }
 
 export class StoredTodoList extends TodoList {
-  readonly id: TodoListId;
+    readonly id: TodoListId;
 
-  constructor(id: TodoListId, name: string, tasks: Task[]) {
-    super(name, tasks);
-    this.id = id;
-  }
+    constructor(id: TodoListId, name: string, tasks: Task[]) {
+        super(name, tasks);
+        this.id = id;
+    }
 }
 
 export function isStoredTodoList(todoList: any): todoList is StoredTodoList {
-  return todoList instanceof StoredTodoList;
+    return todoList instanceof StoredTodoList;
 }
---
+++ src/utils/error/CleanPocErrors.ts
@@ -0,0 +1,23 @@
+export enum CLEANPOC_ERROR {
+  ENTITY_STATUS_ERROR = 'ENTITY_STATUS_ERROR',
+  ENTITY_NOT_FOUND = 'ENTITY_NOT_FOUND',
+}
+
+export const DefaultErrorMessage: {
+  [x in keyof typeof CLEANPOC_ERROR]: string;
+} = {
+  [CLEANPOC_ERROR.ENTITY_STATUS_ERROR]: "Error in entity's internal status",
+  [CLEANPOC_ERROR.ENTITY_NOT_FOUND]: 'Entity not found',
+};
+
+export class CleanPocError implements Error {
+  name: string;
+  message: string;
+  stack?: string;
+
+  constructor(type: CLEANPOC_ERROR, message?: string, stack?: string) {
+    this.name = type;
+    this.message = message || DefaultErrorMessage[type];
+    this.stack = stack;
+  }
+}
