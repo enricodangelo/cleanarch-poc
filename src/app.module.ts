fc42ffce1a9d7101e74c1157aea5e6a9dd17ee81
Enrico D'Angelo <enricodangelo@protonmail.com>
2022-04-12 20:52:18 +0100

    WIP

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { TypeOrmConfigService } from './infrastructure/db/typeormConfiguration.service';
 import { RestApiModule } from './infrastructure/restapi/http.module';
 import { ConfigurationService } from './utils/configuration/configuration.service';
 import { UtilsModule } from './utils/utils.module';
 
 @Module({
-    imports: [UtilsModule, RestApiModule],
+    imports: [
+        UtilsModule,
+        RestApiModule,
+        TypeOrmModule.forRootAsync({
+            imports: [UtilsModule, Connection],
+            inject: [ConfigurationService, Connection],
+            useClass: TypeOrmConfigService,
+        }),
+    ],
 })
 export class AppModule {}
 // constructor(private connection: Connection) {}
